824bf40faaa943d6d443f7f9cd8fb99f329fbe70
joshua <joshua@tier27.com>
2015-07-18 11:21:24 -0400

    Added offset-coordinate board representation

---
+++ board.py
@@ -0,0 +1,22 @@
+# Board representation according to an offset-coordinate system, as described at http://www.redblobgames.com/grids/hexagons/
+GAME_BOARD_COORDINATES = [
+        [-1, -2], # Top left
+        [0, -2],
+        [1, -2], # Top right
+        [-2, -1],
+        [-1, -1],
+        [0, -1],
+        [1, -1],
+        [-2, 0],
+        [-1, 0],
+        [0, 0], # Center
+        [1, 0],
+        [2, 0],
+        [-2, 1],
+        [-1, 1],
+        [0, 1],
+        [1, 1],
+        [-1, 2], # Bottom left
+        [0, 2],
+        [1, 2], # Bottom right
+]
